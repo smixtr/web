398e8e9be0d9e370daa019296fe2c8f66eed0149
Oleksandr Novak <simbol00@gmail.com>
2016-06-04 00:20:00 +0100

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -27,7 +27,7 @@ var Router = Backbone.Router.extend({
     'profile': 'profile',
     ':user': 'feed',
     "facebook/:id": 'facebook',
-    "tumblr/:id": 'tumblr',
+    "tumblr/:id" : 'tumblr',
     '*notFound': 'index'
   },
 
@@ -36,11 +36,80 @@ var Router = Backbone.Router.extend({
     var self = this;
     this.on('route', function(e) {
       var self = this;
-      self.home = $('#content').html(new HomeView().render().el);
       self.footer = $('#footer').html(new FooterView().render().el);
     });
-  }
+  },
 
+  profile: function() {
+    var self = this;
+    var profileView = new ProfileView();
+    self.showView(profileView, $('#content'));
+  },
+
+  feed: function(user) {
+    var self = this;
+
+    var userModel = new User(user);
+
+    templateLoader.load(['FeedView'], function() {
+      userModel.fetch(function() {
+        var v = new FeedView({
+          model: userModel
+        });
+        self.showView(v, $('#content'));
+      });
+    });
+  },
+
+  facebook: function(user){
+    var self=this;
+    console.log('entrou aqui');
+    var userModel = new FacebookPosts(user);
+
+    templateLoader.load(['FacebookView'], function() {
+        userModel.fetch(function() {
+          var v = new FacebookView({
+            model: userModel
+          });
+          self.showView(v, $('#content'));
+        });
+      });
+  },
+
+  tumblr: function(user){
+    var self=this;
+    console.log('entrou aqui');
+    var userModel = new TumblrPosts(user);
+
+    templateLoader.load(['TumblrView'], function() {
+        userModel.fetch(function() {
+          var v = new TumblrView({
+            model: userModel
+          });
+          self.showView(v, $('#content'));
+        });
+      });
+  },
+
+  home: function() {
+    var self = this;
+    var homeView = new HomeView({
+      model: window.profile
+    });
+    self.showView(homeView, $('#content'));
+  },
+
+  verifyLogin: function(loggedFunction) {
+    var self = this;
+    if (!getKeyo()) {
+      app.navigate('/login', {
+        trigger: true
+      });
+    } else {
+      window.logged = true;
+      loggedFunction();
+    }
+  }
 });
 
 templateLoader.load(['FooterView', 'HomeView', 'ProfileView'], function() {
