0d6632462c1104897c94d677e45afcf1c680cbb3
Oleksandr Novak <simbol00@gmail.com>
2016-06-03 23:53:31 +0100

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -5,48 +5,45 @@ Backbone.View.prototype.close = function() {
 };
 
 var Router = Backbone.Router.extend({
-    footer: undefined,
-    currentView: undefined,
-    showView: function(view, elem, sub) {
-      elem.show();
+  footer: undefined,
+  currentView: undefined,
+  showView: function(view, elem, sub) {
+    elem.show();
 
-      if (this.currentView) {
-        this.currentView.close();
-      }
-      this.currentView = view;
-      this.currentView.delegateEvents();
+    if (this.currentView) {
+      this.currentView.close();
+    }
+    this.currentView = view;
+    this.currentView.delegateEvents();
 
-      var rendered = view.render();
-      elem.html(rendered.el);
-    },
+    var rendered = view.render();
+    elem.html(rendered.el);
+  },
 
-    routes: {
-      '': 'home',
-      'index': 'home',
-      'home': 'home',
-      'profile': 'profile',
-      ':user': 'feed',
-      "facebook/:id": 'facebook',
-      "tumblr/:id": 'tumblr',
-      '*notFound': 'index'
-    },
+  routes: {
+    '': 'home',
+    'index': 'home',
+    'home': 'home',
+    'profile': 'profile',
+    ':user': 'feed',
+    "facebook/:id": 'facebook',
+    "tumblr/:id": 'tumblr',
+    '*notFound': 'index'
+  },
 
 
-    initialize: function() {
+  initialize: function() {
+    var self = this;
+    this.on('route', function(e) {
       var self = this;
-      this.on('route', function(e) {
-        var self = this;
-        self.home = $('#content').html(new HomeView().render().el);
-        self.footer = $('#footer').html(new FooterView().render().el);
-      });
-    },
+      self.home = $('#content').html(new HomeView().render().el);
+      self.footer = $('#footer').html(new FooterView().render().el);
+    });
+  }
 
+});
 
-
-
-  }),
-
-  templateLoader.load(['FooterView', 'HomeView', 'ProfileView'], function() {
-    app = new Router();
-    Backbone.history.start();
-  });
+templateLoader.load(['FooterView', 'HomeView', 'ProfileView'], function() {
+  app = new Router();
+  Backbone.history.start();
+});
