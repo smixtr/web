33b141cc37aa1dbdc191f8b9a28c378a674a2b85
dariojorge <jorge12dario12@hotmail.com>
2016-06-03 23:50:11 +0100

    a
    a

--- workers/twitter.js
+++ workers/twitter.js
@@ -1,9 +1,12 @@
 var vendors = require('../vendors'),
-  twitter = require('twitter');
+  Twit  = require('twit');
 
-var Worker = function() {};
+var Worker = function() {
+	this.client = new Twit({});
+};
+var old = undefined;
 
-Worker.prototype.starta = function() {
+Worker.prototype.start = function() {
   var self = this;
   setInterval(function() {
     var cursor = vendors.mongo.collection('users').find({
@@ -14,29 +17,30 @@ Worker.prototype.starta = function() {
     var found = [];
     cursor.each(function(err, doc) {
       if (doc) {
-        this.client = twitter.createClient({
+        self.client({
           consumer_key: process.env.TWITTER_KEY,
           consumer_secret: process.env.TWITTER_SECRET,
-          token: doc.twitterOauthAccessToken,
-          token_secret: doc.twitterOauthAccessTokenSecret
+		  access_token:         doc.twitterOauthAccessToken,
+		  access_token_secret:  doc.twitterOauthAccessTokenSecret,
         });
-
-        this.client.userInfo(function(err, data) {
-          data.user.blogs.forEach(function(blog) {
-            console.log(blog.name);
-            client.posts(blog.name, function(err, resp) {
-              console.log(resp.posts);
-              for (var i = 0; i < resp.posts.length; i++) {
-                self.verifyPost(doc._id, resp.posts[i]);
-              }
-            });
-          });
-        });
+		client.setAuth(client);
+		client.get('statuses/user_timeline', function(err, data, response) {
+		  if(!err)
+			  if(old === undefined){
+				  old = data[0].text;
+				  console.log(old);
+			  }else if(old !== data[0].text){
+				old = data[0].text;
+				console.log(old);
+			  }
+		});
       }
     });
-  }, 60000);
+  }, 6000);
 };
 
+//TWITTER_KEY=EKT9YuYCDMTd6sFoIHVgIM6Gk
+//TWITTER_SECRET=vuSSMQ7C6Vr2J9VJwH44WPsFgSO8xnzohIdNowfm68GJEg0GA4
 Worker.prototype.verifyPost = function(userid, post) {
   vendors.mongo.collection('users').findOne({
     _id: userid,
